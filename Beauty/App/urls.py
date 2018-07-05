bbd44983238b7bec45752390ec6aba1ea1c61166
Mr.Zhang <zhangli9479@163.com>
2018-07-05 20:20:10 +0800

    itme2018-7-5

--- Beauty/App/urls.py
+++ Beauty/App/urls.py
@@ -1,6 +1,15 @@
+from django.conf.urls import url
 
-
+from App import views
 
 urlpatterns = [
-    #url()
+    url(r'^classify/$', views.classify, name='classify'),
+    url(r'^classify_list/(\d+)/$', views.classify_list, name='classify_list'),
+    url(r'^show/(\d+)/', views.show, name='show'),
+    url(r'^subgoods/',views.subgoods),
+    url(r'^addgoods/',views.addgoods),
+    url(r'^add_cart',views.add_cart),
+    url(r'^cart/',views.cart),
+    url(r'^buy/',views.buy,name='buy'),
+    url(r'^order/',views.order),
 ]
