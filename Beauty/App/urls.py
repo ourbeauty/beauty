310f641e07fdbd6dcf7831aa4e74a62fbe044a60
ljl <344887562@qq.com>
2018-07-06 14:25:46 +0800

    最后提交

--- Beauty/App/urls.py
+++ Beauty/App/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from App import views
 
 urlpatterns = [
-    url(r'dizhi/', views.dizhi, name='dizhi'),
-    url(r'adddizhi/', views.adddizhi, name='adddizhi')
+    url(r'adddizhi/$', views.adddizhi, name='adddizhi'),
+    url(r'changedizhi/(?P<id>\d+)/$', views.changedizhi, name='changedizhi'),
+    url(r'deldizhi/(\d+)/$', views.deldizhi, name='deldizhi'),
+    url(r'dizhi/$', views.dizhi, name='dizhi')
 ]
